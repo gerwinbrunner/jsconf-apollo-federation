2396cf8e29237a8ce44dfb111e374c86d40d4a88
Gerwin Brunner <gerwin.brunner@vilango.com>
2019-08-14 10:03:43 -0700

    improved presentation flow

--- app-pre/imports/startup/server/index.js
+++ app-pre/imports/startup/server/index.js
@@ -1,7 +1,12 @@
+import { Meteor } from "meteor/meteor";
 import _ from "lodash";
 
 import { ApolloServer } from "apollo-server-express";
 import { WebApp } from "meteor/webapp";
+// import { ApolloGateway } from "@apollo/gateway";
+// import { initializeApolloServer as initCustomers } from "/imports/microservices/customer";
+// import { initializeApolloServer as initCoffee } from "/imports/microservices/coffee";
+// import { initializeApolloServer as initOrder } from "/imports/microservices/order";
 
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
@@ -21,3 +26,42 @@ WebApp.connectHandlers.use("/graphql", (req, res) => {
     res.end();
   }
 });
+
+// // const customerPath = "/gms/customer";
+// // const coffeePath = "/gms/coffee";
+// // const orderPath = "/gms/order";
+
+// // initCustomers({ path: customerPath });
+// // initCoffee({ path: coffeePath });
+// // initOrder({ path: orderPath });
+
+// Meteor.startup(() => {
+//   Meteor.defer(() => {
+//     const gateway = new ApolloGateway({
+//       serviceList: [
+//         // { name: "customer", url: getUrl({ path: customerPath }) },
+//         // { name: "coffee", url: getUrl({ path: coffeePath }) },
+//         // { name: "order", url: getUrl({ path: orderPath }) },
+//       ],
+//       debug: true,
+//     });
+//     const { schema, executor } = Promise.await(gateway.load());
+//     const serverGateway = new ApolloServer({
+//       schema,
+//       executor,
+//     });
+//     serverGateway.applyMiddleware({
+//       app: WebApp.connectHandlers,
+//       path: "/graphql",
+//     });
+//     WebApp.connectHandlers.use("/graphql", (req, res) => {
+//       if (req.method === "GET") {
+//         res.end();
+//       }
+//     });
+//   });
+// });
+
+// const getUrl = ({ path }) => {
+//   return _.trimEnd(Meteor.absoluteUrl(), "/") + path;
+// };
