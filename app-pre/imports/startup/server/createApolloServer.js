14d593265824246138ae0cff6411dc7adaf56403
Gerwin Brunner <gerwin.brunner@vilango.com>
2019-08-14 07:38:50 -0700

    cleanup

---
+++ app-pre/imports/startup/server/createApolloServer.js
@@ -0,0 +1,27 @@
+import { buildFederatedSchema } from "@apollo/federation";
+import { ApolloServer } from "apollo-server-express";
+import { WebApp } from "meteor/webapp";
+
+export const createApolloServer = ({ path, typeDefs, resolvers }) => {
+  const schema = buildFederatedSchema([
+    {
+      typeDefs: typeDefs,
+      resolvers: resolvers,
+    },
+  ]);
+
+  const server = new ApolloServer({
+    schema,
+  });
+  server.applyMiddleware({
+    app: WebApp.connectHandlers,
+    path,
+  });
+  WebApp.connectHandlers.use(path, (req, res) => {
+    if (req.method === "GET") {
+      res.end();
+    }
+  });
+  console.log("createApolloServer", path);
+  return { server, schema };
+};
